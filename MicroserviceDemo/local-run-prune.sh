28acc237f4b329591077dda93c92e239a5faa46b
nbnbnb <nbnbnb.zhang@gmail.com>
2021-11-28 18:16:24 +0800

    prune

--- MicroserviceDemo/local-run-prune.sh
+++ MicroserviceDemo/local-run-prune.sh
@@ -1,3 +1,5 @@
+docker container prune -f
+docker image prune -f
 docker-compose -f docker-compose-local.yml build
 docker-compose -f docker-compose-local.yml down
 docker-compose -f docker-compose-local.yml up -d
