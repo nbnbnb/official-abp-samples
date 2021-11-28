b0c14f8f33f35c734c86baec37394bfb593cedd9
nbnbnb <nbnbnb.zhang@gmail.com>
2021-11-28 20:48:17 +0800

    prune

--- MicroserviceDemo/local-run-prune.bat
+++ MicroserviceDemo/local-run-prune.bat
@@ -1,5 +1,5 @@
-docker container prune -f
-docker image prune -f
 docker-compose -f docker-compose-local.yml build
 docker-compose -f docker-compose-local.yml down
 docker-compose -f docker-compose-local.yml up -d
+docker container prune -f
+docker image prune -f
