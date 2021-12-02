05d98fafc688243b266416c01968dc96779b4b28
张进 <jinzhanga@trip.com>
2021-12-02 16:16:10 +0800

    Revert "clean libs"
    This reverts commit 95c1e7922a3ffd5222ddbd6d0ae7cb018233a7fb.

--- MicroserviceDemo/applications/PublicWebSite.Host/gulpfile.js
+++ MicroserviceDemo/applications/PublicWebSite.Host/gulpfile.js
@@ -4,6 +4,6 @@ var gulp = require("gulp"),
     path = require('path'),
     copyResources = require('./node_modules/@abp/aspnetcore.mvc.ui/gulp/copy-resources.js');
 
-gulp.task('default', async function () {
-    await copyResources(path.resolve('./'));
-});
+exports.default = function(){
+    return copyResources(path.resolve('./'));
+};
--- MicroserviceDemo/applications/PublicWebSite.Host/wwwroot/libs/abp/jquery/abp.jquery.js
+++ MicroserviceDemo/applications/PublicWebSite.Host/wwwroot/libs/abp/jquery/abp.jquery.js
@@ -99,28 +99,19 @@ var abp = abp || {};
         options.success = undefined;
         options.error = undefined;
 
-        var xhr = null;
-        var promise = $.Deferred(function ($dfd) {
-            xhr = $.ajax(options)
+        return $.Deferred(function ($dfd) {
+            $.ajax(options)
                 .done(function (data, textStatus, jqXHR) {
                     $dfd.resolve(data);
                     userOptions.success && userOptions.success(data);
                 }).fail(function (jqXHR) {
-                    if(jqXHR.status === 0 || jqXHR.statusText === 'abort') {
-                        //ajax request is abort, ignore error handle.
-                        return;
-                    }
                     if (jqXHR.getResponseHeader('_AbpErrorFormat') === 'true') {
                         abp.ajax.handleAbpErrorResponse(jqXHR, userOptions, $dfd);
                     } else {
                         abp.ajax.handleNonAbpErrorResponse(jqXHR, userOptions, $dfd);
                     }
                 });
-        }).promise();
-
-        promise['jqXHR'] = xhr;
-
-        return promise;
+        });
     };
 
     $.extend(abp.ajax, {
