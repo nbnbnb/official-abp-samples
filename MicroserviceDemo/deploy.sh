e3bbbcd4fb01a0a7bd03063e8d0cdba4aa3f711a
JJZhang <seoul-arm@zhangjin.tk>
2021-12-08 00:42:21 +0800

    add deploy.sh

---
+++ MicroserviceDemo/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-authserver-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminapp-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsite-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-identityservice-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-tenantmanagementservice-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-bloggingservice-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-productservice-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-tnternalgateway-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminappgateway-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsitegateway-host
+
+docker-compose -f docker-compose-arm.yml down
+docker-compose -f docker-compose-arm.yml up -d
