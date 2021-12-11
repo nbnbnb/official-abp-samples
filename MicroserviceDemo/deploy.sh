4db4503179948b9cbb9aad8de18337192f0ea79e
nbnbnb <nbnbnb.zhang@gmail.com>
2021-12-11 13:30:03 +0800

    single deploy

--- MicroserviceDemo/deploy.sh
+++ MicroserviceDemo/deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-authserver-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminapp-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsite-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-identityservice-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-tenantmanagementservice-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-bloggingservice-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-productservice-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-internalgateway-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminappgateway-host
-docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsitegateway-host
+docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
 
-docker-compose -f docker-compose-arm.yml down
-docker-compose -f docker-compose-arm.yml up -d
+docker-compose -f docker-compose-arm.yml down $1
+docker-compose -f docker-compose-arm.yml up -d $1
