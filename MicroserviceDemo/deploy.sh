bce848f3d05907a87c9344064335a92d90515a16
nbnbnb <nbnbnb.zhang@gmail.com>
2021-12-11 15:11:17 +0800

    cc

--- MicroserviceDemo/deploy.sh
+++ MicroserviceDemo/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Deploy Param01 $1" 
+echo "Deploy Param02 $2" 
+
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
 
 docker-compose -f docker-compose-arm.yml down $1
