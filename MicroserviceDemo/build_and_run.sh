b555842bf5711ec44247358e71253cc42313e7be
JJZhang <dashan1010zj@163.com>
2022-04-05 21:10:41 +0800

    cc

--- MicroserviceDemo/build_and_run.sh
+++ MicroserviceDemo/build_and_run.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose-aliyun.yml down
 docker-compose -f docker-compose-aliyun.yml --env-file ./.env.arm build
-docker-compose -f docker-compose-aliyun.yml -f docker-compose-aliyun.yml up --force-recreat -d
+docker-compose -f docker-compose-aliyun.yml --env-file ./.env.arm -f docker-compose-aliyun.yml up --force-recreat -d
