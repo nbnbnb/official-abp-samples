61f8ec7ba317ab7557400a1b8e370bfc42721908
JJZhang <dashan1010zj@163.com>
2022-04-06 22:13:29 +0800

    Seq

--- MicroserviceDemo/applications/AuthServer.Host/Program.cs
+++ MicroserviceDemo/applications/AuthServer.Host/Program.cs
@@ -28,6 +28,7 @@ namespace AuthServer.Host
                 .Enrich.WithProperty("Application", "AuthServer")
                 .Enrich.FromLogContext()
                 .WriteTo.File("Logs/logs.txt")
+                .WriteTo.Seq(configuration["Seq:Url"])
                 .WriteTo.Elasticsearch(
                     new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Url"]))
                     {
