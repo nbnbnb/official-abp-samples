2e3bd9179b5c1018f6dbdb723efb3bbef56cf50d
JJZhang <dashan1010zj@163.com>
2022-04-08 22:16:02 +0800

    clean

--- MicroserviceDemo/build_and_run.sh
+++ MicroserviceDemo/build_and_run.sh
@@ -2,6 +2,5 @@ git reset --hard
 git clean -df
 git pull
 docker-compose -f docker-compose-aliyun.yml build
-docker-compose -f docker-compose-aliyun.yml down
 docker-compose -f docker-compose-aliyun.yml --env-file ./.env.arm up --force-recreat -d
 docker image prune -f
