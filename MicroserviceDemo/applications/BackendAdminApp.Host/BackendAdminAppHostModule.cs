065862501c1a31aae148c2eecb4a16b8687d6700
JJZhang <dashan1010zj@163.com>
2022-04-08 20:35:58 +0800

    Revert "Revert "logs""
    This reverts commit d5c8b8b9aee52dbb0fe7ecc3db60ba89bd57fe35.

--- MicroserviceDemo/applications/BackendAdminApp.Host/BackendAdminAppHostModule.cs
+++ MicroserviceDemo/applications/BackendAdminApp.Host/BackendAdminAppHostModule.cs
@@ -28,6 +28,8 @@ using Volo.Blogging;
 using Microsoft.AspNetCore.Authorization;
 using Volo.Abp.AspNetCore.ExceptionHandling;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace BackendAdminApp.Host
 {
@@ -54,6 +56,20 @@ namespace BackendAdminApp.Host
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             var configuration = context.Services.GetConfiguration();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", "BackendAdminApp")
+                .Enrich.FromLogContext()
+                .WriteTo.Seq(configuration["Seq:Url"])
+                .WriteTo.File("Logs/logs.txt")
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Url"]))
+                    {
+                        AutoRegisterTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                        IndexFormat = "msdemo-log-{0:yyyy.MM}"
+                    })
+                .CreateLogger();
 
             Configure<AbpLocalizationOptions>(options =>
             {
