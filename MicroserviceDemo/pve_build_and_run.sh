0a6fe984f79e49b8a25521e9f1c1ca09d22f8af8
JJZhang <dashan1010zj@163.com>
2022-04-10 16:45:32 +0800

    clean arm pve

--- MicroserviceDemo/pve_build_and_run.sh
+++
@@ -1,6 +0,0 @@
-git reset --hard 
-git clean -df
-git pull
-docker-compose -f pve-docker-compose.yml build
-docker-compose -f pve-docker-compose.yml --env-file ./.env.pve up --force-recreat -d
-docker image prune -f
