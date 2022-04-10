7cb19fd8a8f3e5de517a0151e4a45198e99dcbe8
JJZhang <dashan1010zj@163.com>
2022-04-10 23:43:57 +0800

    cc

--- MicroserviceDemo/pull_and_run.sh
+++ MicroserviceDemo/pull_and_run.sh
@@ -3,33 +3,33 @@
 profile=${1}
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-authserver-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat auth-server -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d auth-server
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminapp-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat backend-admin-app -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d backend-admin-app
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsite-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat public-website -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d public-website
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-identityservice-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat identity-service -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d identity-service
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-tenantmanagementservice-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat tenant-management-service -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d tenant-management-service
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-bloggingservice-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat logging-service -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d blogging-service 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-productservice-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat product-service -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d product-service 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-internalgateway-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat internal-gateway -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d internal-gateway 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-backendadminappgateway-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat backend-admin-app-gateway -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d backend-admin-app-gateway 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:abp-publicwebsitegateway-host
-docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat public-website-gateway -d
+docker-compose -f aliyun-docker-compose.yml --env-file .env.${profile} up --force-recreat -d public-website-gateway 
 
 docker image prune -f
