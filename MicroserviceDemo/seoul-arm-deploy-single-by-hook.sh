8e7d6da632a40139e3675f97d4d8119d3e4ed228
JJZhang <dashan1010zj@163.com>
2022-04-10 16:44:57 +0800

    aliyun

--- MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
+++ MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
@@ -4,6 +4,5 @@ echo "Deploy Param01 $1"
 echo "Deploy Param02 $2" 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
-docker-compose --env-file ./.env.arm -f arm-docker-compose.yml up -d --force-recreat $1
-
+docker-compose -f aliyun-docker-compose.yml --env-file ./.env.arm  up -d --force-recreat $1
 
