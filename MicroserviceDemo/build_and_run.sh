85afb88723245a3d93ee174faccb5bfe6b9a95f3
JJZhang <dashan1010zj@163.com>
2022-04-10 12:40:19 +0800

    clean

--- MicroserviceDemo/build_and_run.sh
+++
@@ -1,7 +0,0 @@
-profile=$1
-git reset --hard 
-git clean -df
-git pull
-docker-compose -f ${profile}-docker-compose.yml build
-docker-compose -f ${profile}-docker-compose.yml --env-file ./.env.${profile} up --force-recreat -d
-docker image prune -f
