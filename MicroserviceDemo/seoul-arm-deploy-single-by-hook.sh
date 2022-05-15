3ddc2e808c0d835cf914b4e16b7c5f6dd6764948
JJZhang <dashan1010zj@163.com>
2022-05-15 13:31:20 +0800

    Update hook

--- MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
+++ MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Deploy Param01 $1" 
 echo "Deploy Param02 $2" 
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
-docker-compose -f aliyun-docker-compose.yml --env-file .env.arm  up -d --force-recreat $1
+docker-compose -f aliyun-docker-compose.yml --env-file .env.arm up -d --force-recreat $1
 
