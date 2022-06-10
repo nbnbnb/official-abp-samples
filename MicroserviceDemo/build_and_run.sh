ff876ddec9120fc071c0bf95aee1e0607500eb12
JJZhang <dashan1010zj@163.com>
2022-06-10 19:54:14 +0800

    full  git path

--- MicroserviceDemo/build_and_run.sh
+++ MicroserviceDemo/build_and_run.sh
@@ -4,9 +4,10 @@ profile=${1}
 
 echo "profile: ${profile}"
   
-git reset --hard 
-git clean -df
-git pull
+/usr/bin/git reset --hard 
+/usr/bin/git clean -df
+/usr/bin/git pull
+
 docker-compose -f aliyun-docker-compose.yml build
 docker-compose -f aliyun-docker-compose.yml --env-file ./.env.${profile} up -d
 docker image prune -f
--- MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
+++ MicroserviceDemo/seoul-arm-deploy-single-by-hook.sh
@@ -3,8 +3,8 @@
 echo "Deploy Param01 $1" 
 echo "Deploy Param02 $2" 
 
-git checkout master
-git pull
+/usr/bin/git checkout master
+/usr/bin/git pull
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
 docker compose -f aliyun-docker-compose.yml --env-file .env.arm up -d $1
