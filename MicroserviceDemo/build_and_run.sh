8cf8751c4ad5e5f48d01ed3eac076decc4d663a8
JJZhang <dashan1010zj@163.com>
2022-06-10 19:54:56 +0800

    cc

--- MicroserviceDemo/build_and_run.sh
+++ MicroserviceDemo/build_and_run.sh
@@ -8,6 +8,7 @@ echo "profile: ${profile}"
 /usr/bin/git clean -df
 /usr/bin/git pull
 
+
 docker-compose -f aliyun-docker-compose.yml build
 docker-compose -f aliyun-docker-compose.yml --env-file ./.env.${profile} up -d
 docker image prune -f
